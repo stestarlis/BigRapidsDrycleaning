2654b910b71eba5195f448fcc09dabef46f48985
Steven Starlin <steven.starlin@iterix.net>
2024-05-04 13:06:23 -0400

    adding some stuff for release

--- BRDObjects/Statics/Connections.cs
+++ BRDObjects/Statics/Connections.cs
@@ -6,11 +6,11 @@ namespace BRDObjects.Statics
     {
         public static string GetDefaultConnection() => "Server=localhost;Database=BRDryCleaning;Trusted_Connection=True;TrustServerCertificate=True;";
         public static string GetEllisConnection() => "Server=MEDIASERVER\\SQLExpress;Database=BRDryCleaning;Trusted_Connection=true;TrustServerCertificate=true;Integrated Security=false;User ID=readOnlyUser; Password=Cyndaquil2023;";
-
+        public static string GetAzureConnection() => "Server=tcp:brdrycleaning-server.database.windows.net,1433;Initial Catalog=brdrycleaning;Persist Security Info=False;User ID=brdrycleaning-server-admin;Password=6mO7$SYMqrI9UNA$;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
         public static string GetConnection()
         {
             var useEllisConnection = PingHost("MEDIASERVER");
-            return useEllisConnection ? GetEllisConnection() : GetDefaultConnection();
+            return useEllisConnection ? GetEllisConnection() : GetAzureConnection();
         }
 
         public static bool PingHost(string nameOrAddress)
